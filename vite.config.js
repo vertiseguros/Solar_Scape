c0fa3b4e64351942656776e325a10c26792e2277
albertocarronovo <alberto.carronovo@students.iaac.net>
2026-03-13 10:36:46 +0100

    Build docs output for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,9 @@ import vue from '@vitejs/plugin-vue';
 export default defineConfig({
   base: '/Solar_Scape/',
   plugins: [vue()],
+  build: {
+    outDir: 'docs',
+  },
   server: {
     host: true,
   },
